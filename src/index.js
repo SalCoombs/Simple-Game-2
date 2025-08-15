771fd3a337fc68b67a935b0fb31f8407f42c624d
SalCoombs <sal@waldegrave.org>
2025-08-15 10:06:15 -0700

    Linked Enemy, Player and Key handler classes.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+import Enemy from "./enemy.js";
 import EventSystem from "./eventSystem.js";
 import HtmlHandler from "./htmlHandler.js";
+import KeyHandler from "./keyPressHandler.js";
+import Player from "./player.js";
 
 window.onload = initGame;
 
 function initGame() {
   const eventSystem = new EventSystem();
   const htmlHandler = new HtmlHandler(eventSystem);
+  const keyHandler = new KeyHandler(eventSystem);
+  const player = new Player(eventSystem);
+  const enemy = new Enemy(eventSystem);
+
+  requestAnimationFrame(runGame);
+}
+
+function runGame() {
+  requestAnimationFrame(runGame);
 }
