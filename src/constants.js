1327fc1bce82e580d978fc3e397433fd8f68f001
SalCoombs <sal@waldegrave.org>
2025-08-15 10:05:17 -0700

    Added constants.

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,20 @@
 export const eventTypes = Object.freeze({
   DISPLAY_TEXT: "display-text",
+  KEY_PRESSED: "key-pressed",
+  PLAYER_ATTACK: "player-attack",
+  // ATTACK: "attack",
+  // J_PRESSED: "j-pressed",
+});
+
+export const keyBinds = Object.freeze({
+  J: "J",
+  K: "K",
+  I: "I",
+  P: "P",
+});
+
+export const enemyTypes = Object.freeze({
+  WATER: "water",
+  EARTH: "earth",
+  FIRE: "fire",
 });
